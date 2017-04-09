72d99c5f5b6f093025f0a7de14ec3fcc699cc0f0
pp <584082450@qq.com>
2017-04-09 17:33:26 +0800

    讲师列表查看&修改&编辑&日期插件

--- js/course/course_category_add.js
+++ js/course/course_category_add.js
@@ -1,3 +1,7 @@
-define([], function() {
+define(['jquery','common','header','aside','nprogress','loading'], function($,undefined,undefined,undefined,nprogress,loading) {
 	
+
+  console.log(common.parseSearch());
+  console.log(common.parseSearch('cg_id'));
+  nprogress.done();
 });
--- js/teacher/teacher_list.js
+++ js/teacher/teacher_list.js
@@ -1,10 +1,47 @@
-define(['aside', 'header', 'loading', 'nprogress', 'jquery', 'template'], function (undefined, undefined, undefined, nprogress, $, template) {
+define(['bootstrap', 'aside', 'header', 'loading', 'nprogress', 'jquery', 'template'], function (undefined, undefined, undefined, undefined, nprogress, $, template) {
   //  渲染讲师列表
   (function () {
     $.get('/v6/teacher', function (data) {
       if (data.code === 200) {
         $('.table-teacher-list').append(template('teacher-list-tpl', data))
       }
+    })
+  })();
+
+  //  渲染讲师列表详情
+  (function () {
+    $(document).on('click', '.teacher-view', function () {
+      $.get('/v6/teacher/view', {
+        tc_id: $(this).data('teacher-id')
+      }, function (data) {
+        if (data.code === 200) {
+          $('#teacher-modal').html(template('teacher-view-tpl', data.result))
+        }
+      })
+    });
+  })();
+
+  // 注销启动讲师
+  (function () {
+    // 给document绑定事件委托触发teacher-handle
+    $(document).on('click', '.teacher-handle', function () {
+      var self = this;
+      $.post('/v6/teacher/handle', {
+          tc_id: $(this).data('teacher-id'),
+          tc_status: $(this).data('teacher-status')
+        },function (data) {
+
+          if (data.code == 200) {
+
+            if (data.result.tc_status == 0) {
+              $(self).data('teacher-status', 0);
+              $(self).html('注销');
+            } else {
+              $(self).data('teacher-status', 1);
+              $(self).html('启用');
+            }
+          }
+        })
     })
   })();
   nprogress.done();
